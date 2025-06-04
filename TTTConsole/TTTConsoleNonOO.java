f98111844311011fbf189f9250b578b02290630c
Gus Satria <satria070906@gmail.com>
2025-06-04 17:47:05 +0700

    COMMIT

--- TTTConsole/TTTConsoleNonOO.java
+++ TTTConsole/TTTConsoleNonOO.java
@@ -35,6 +35,7 @@ public class TTTConsoleNonOO {
             // Initialize the board, currentState and currentPlayer
             initGame();
 
+
             // Play the game once
             do {
                 // currentPlayer makes a move
